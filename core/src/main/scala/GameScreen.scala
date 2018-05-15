fe75ce50372ded20e16ed14fb4faca14a3a6a94c
Adam Ramos <amramos0828@gmail.com>
2018-05-15 13:22:23 -0400

    setup scalaxy to run with a gamescreen

--- core/src/main/scala/GameScreen.scala
+++ core/src/main/scala/GameScreen.scala
@@ -1,28 +1,41 @@
 package com.angry.micro.raptor.scalaxy
 
 import com.badlogic.gdx.Screen
-import com.badlogic.gdx.graphics.{OrthographicCamera, Texture}
+import com.badlogic.gdx.graphics.{GL20, OrthographicCamera, Texture}
+import com.badlogic.gdx.math.Rectangle
 
 /**
   * Created by amram on 5/14/2018.
   */
 class GameScreen(scalaxy: Scalaxy) extends Screen {
-  private var img: Texture = new Texture("badlogic.jpg")
-  private var camera: OrthographicCamera = new OrthographicCamera()
-  this.camera.setToOrtho(true, 800, 480)
+    private val img: Texture = new Texture("badlogic.jpg")
+    private val camera: OrthographicCamera = new OrthographicCamera()
+    this.camera.setToOrtho(false, 800, 480)
 
-  override def resume(): Unit = ???
+    private val rect = new Rectangle()
 
-  override def show(): Unit = ???
+    override def resume(): Unit = {}
+    override def show(): Unit = {}
+    override def pause(): Unit = {}
+    override def hide(): Unit = {}
+    override def resize(width: Int, height: Int): Unit = {}
+    override def dispose(): Unit = this.img.dispose()
 
-  override def pause(): Unit = ???
+    override def render(delta: Float) {
+        import com.badlogic.gdx.Gdx
+        // clear the screen with a dark blue color. The// clear the screen with a dark blue color. The
+        // arguments to glClearColor are the red, green
+        // blue and alpha component in the range [0,1]
+        // of the color to be used to clear the screen.
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-  override def hide(): Unit = ???
+        this.camera.update()
+        scalaxy.batch.setProjectionMatrix(this.camera.combined)
 
-  override def resize(width: Int, height: Int): Unit = ???
-
-  override def dispose(): Unit = ???
-
-  override def render(delta: Float): Unit = ???
+        scalaxy.batch.begin()
+        scalaxy.batch.draw(this.img, 0, 20)
+        scalaxy.batch.end()
+    }
 }
 
--- core/src/main/scala/Scalaxy.scala
+++ core/src/main/scala/Scalaxy.scala
@@ -3,9 +3,7 @@ package com.angry.micro.raptor.scalaxy
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.physics.box2d.World
-import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
@@ -23,11 +21,21 @@ class Scalaxy extends Game {
         this.camera = new OrthographicCamera(32, 24)
         this.debugRenderer = new Box2DDebugRenderer(true, true, true, true, true, true)
         this.world = new World(new Vector2(0,-10f), true)
+
+        this.setScreen(new GameScreen(this))
     }
 
     override def render() {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        this.debugRenderer.render(this.world, this.camera.combined)
+//        Gdx.gl.glClearColor(0f, 0f, 0f, 1)
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+//        this.debugRenderer.render(this.world, this.camera.combined)
+        super.render()
+    }
+
+    override def dispose() {
+        this.batch.dispose()
+        this.img.dispose()
+        this.debugRenderer.dispose()
+        this.world.dispose()
     }
 }
